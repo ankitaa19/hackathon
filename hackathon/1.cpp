07f11d8890dc22dd9d3682ad8412c04256e97a3f
Ankita Lokhande <142775992+ankitaa19@users.noreply.github.com>
2024-05-02 10:52:08 +0530

    Add files via upload

---
+++ hackathon/1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool isUgly(int n) {
+    if (n <= 0) return false;
+    
+    while (n % 2 == 0) {
+        n /= 2;
+    }
+    while (n % 3 == 0) {
+        n /= 3;
+    }
+    while (n % 5 == 0) {
+        n /= 5;
+    }
+    
+    return n == 1;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (isUgly(n)) {
+        cout << n << " is an ugly number." << endl;
+    } else {
+        cout << n << " is not an ugly number." << endl;
+    }
+    return 0;
+}
+
